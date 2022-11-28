97b1a0562a8add62bdb50f540cb9e85182db7fb5
magla <mag.lauritzen@gmail.com>
2022-11-28 22:00:29 +0100

    More tests.

--- src/tests/test.py
+++ src/tests/test.py
@@ -8,31 +8,32 @@ import time
 from time import perf_counter
 import unittest
 import logging
-from src.threaded_videocapture import ThreadedVideoCapture
+logging.basicConfig(level=logging.DEBUG)
+from src.threaded_videocapture import ThreadedVideoCapture, module_logger
 
 logger = logging.getLogger("tests")
-logger.setLevel(logging.DEBUG)
-
 
 class TestThreadedVideoCapture(unittest.TestCase):
     def assert_normal_behavior(self, tvc: ThreadedVideoCapture):
         assert tvc.isOpened()
+        assert tvc.is_alive
         timeout = tvc.timeout
         get_frame_within = perf_counter() + timeout / 2 if timeout > 0 else perf_counter() + 1
-        assert get_frame_within > 0
+        assert get_frame_within > 0, f"get_frame_within={get_frame_within}"
         while perf_counter() < get_frame_within:
             ret, frame = tvc.read()
             if ret:
                 break
-        assert ret
-        assert isinstance(frame, np.ndarray)
+        assert ret, f"ret={ret}"
+        assert isinstance(frame, np.ndarray), f"type of frame={type(frame)}"
         ret, frame = tvc.read()
-        assert not ret
-        assert frame is None
-        time.sleep(tvc.timeout + 0.1)
+        assert not ret, f"ret={ret}"
+        assert frame is None, f"frame={frame}"
+        time.sleep(tvc.timeout + 1)
+        assert not tvc.is_alive
         ret, frame = tvc.read()
-        assert ret is None
-        assert frame is None
+        assert ret is None, f"ret={ret}"
+        assert frame is None, f"frame={frame}"
 
     def test_basic(self):
         timeout = 0.2
@@ -81,18 +82,39 @@ class TestThreadedVideoCapture(unittest.TestCase):
         with ThreadedVideoCapture("testimage_1.jpg") as tvc:
             tvc.logger.setLevel(logging.DEBUG)
             tvc.logger.addHandler(handler)
-            pass
         assert log_capture_string.getvalue() == "Received QUIT signal.\n", f"Contents is " \
                                                                            f"\"{repr(log_capture_string.getvalue())}\"."
+        tvc.logger.removeHandler(handler)
         log_capture_string.truncate(0)
         log_capture_string.seek(0)
-        with ThreadedVideoCapture("testimage_1.jpg", logger=logger) as tvc:
+        testlogger = logging.getLogger("testlogger")
+        with ThreadedVideoCapture("testimage_1.jpg", logger=testlogger) as tvc:
+            tvc.logger.setLevel(logging.DEBUG)
             tvc.logger.addHandler(handler)
-            pass
         assert log_capture_string.getvalue() == "Received QUIT signal.\n", f"Contents is " \
                                                                            f"\"{repr(log_capture_string.getvalue())}\"."
         log_capture_string.close()
 
+    def test_isalive(self):
+        """ Test that the isalive() method returns expected results. """
+        with ThreadedVideoCapture("testimage_1.jpg") as tvc:
+            assert tvc.is_alive
+        assert not tvc.is_alive
+
+    def test_release(self):
+        """ Test that the release method can close the ThreadedVideoCapture"""
+        with ThreadedVideoCapture("testimage_1.jpg") as tvc:
+            assert tvc.is_alive
+            tvc.release()
+            assert not tvc.is_alive
+
+    def test_open(self):
+        """ Test that we can succesfully re-open a ThreadedVideoCapture that has been released. """
+        with ThreadedVideoCapture("testimage_1.jpg") as tvc:
+            tvc.logger.setLevel(logging.INFO)
+            self.assert_normal_behavior(tvc)
+            assert tvc.open("testimage_1.jpg")
+            self.assert_normal_behavior(tvc)
 
 if __name__ == '__main__':
     unittest.main()
